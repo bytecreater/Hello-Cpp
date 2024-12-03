bf534e46728205eb61b24d460c6042dadde8cb3e
bytecreater <nihalahemad2003@gmail.com>
2024-12-03 15:23:09 +0530

    find no. which is not twice and bitwise operator use

---
+++ single.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// using array
+// int main() {
+//     int n=5;
+//    int arr[n]={4,1,2,1,2};
+//    int x=0;
+//    for (int i=0; i<n; i++){
+//     x=x^arr[i];
+//    }
+//    cout<<x;
+//     return 0;
+// }
+
+// using vectors
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    vector<int>vec={4,2,1,2,1};
+    int x=0;
+    for (int val : vec){
+        x=x^val;
+    }
+    cout<<x;
+    return 0;
+}
